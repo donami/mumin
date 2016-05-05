d73215161de7749fd00f96d0afa0e9f240dd17b0
Markus Hederström <d0namiz@gmail.com>
2016-05-05 22:13:48 +0200

    Test

--- autoloader.php
+++ autoloader.php
@@ -1,16 +1,18 @@
 <?php
 /**
  * An example of a project-specific implementation.
- * 
+ *
  * After registering this autoload function with SPL, the following line
  * would cause the function to attempt to load the \Foo\Bar\Baz\Qux class
  * from /path/to/project/src/Baz/Qux.php:
- * 
+ *
  *      new \Foo\Bar\Baz\Qux;
- *      
+ *
  * @param string $class The fully-qualified class name.
  * @return void
  */
+
+//test
 spl_autoload_register(function ($class) {
 
     // project-specific namespace prefix
